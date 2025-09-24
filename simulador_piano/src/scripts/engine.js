fddbf37fdb498b5789d60203e1e303258dfb6bd9
PMagoga <pmagoga@gmail.com>
2025-09-23 22:35:28 -0300

    adicionando estilização

---
+++ simulador_piano/src/scripts/engine.js
@@ -0,0 +1,19 @@
+const pianoKeys = document.querySelectorAll('.piano-keys .key');
+const keysCheckbox = document.querySelector('.keys-checkbox input');
+
+let audio = new Audio("src/tunes/a.wav");
+
+const playTune = (key) => {
+    audio.src = `src/tunes/${key}.wav`;
+    audio.play();
+    console.log(audio.src)
+};
+
+pianoKeys.forEach((key) => {
+    console.log(key.dataset.key)
+    key.addEventListener('click', () => playTune(key.dataset.key));
+});
+
+document.addEventListener('keydown', (e) => {
+    playTune(e.key);
+});
