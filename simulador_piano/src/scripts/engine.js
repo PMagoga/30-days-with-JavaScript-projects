88f84842687b3233c8ef68fb2158635b4c56ce9d
PMagoga <pmagoga@gmail.com>
2025-09-24 18:03:03 -0300

    finalizando o projeto

--- simulador_piano/src/scripts/engine.js
+++ simulador_piano/src/scripts/engine.js
@@ -3,17 +3,26 @@ const keysCheckbox = document.querySelector('.keys-checkbox input');
 
 let audio = new Audio("src/tunes/a.wav");
 
+const mapedKeys = [];
+
 const playTune = (key) => {
     audio.src = `src/tunes/${key}.wav`;
     audio.play();
-    console.log(audio.src)
+    
+    const clickedKey = document.querySelector(`[data-key="${key}"]`);
+    clickedKey.classList.add('active');
+    setTimeout(() => { clickedKey.classList.remove('active'); }, 150);
 };
 
 pianoKeys.forEach((key) => {
     console.log(key.dataset.key)
     key.addEventListener('click', () => playTune(key.dataset.key));
+    mapedKeys.push(key.dataset.key);
 });
 
 document.addEventListener('keydown', (e) => {
-    playTune(e.key);
+    if (mapedKeys.includes(e.key)){
+        playTune(e.key);
+    }
+    
 });
